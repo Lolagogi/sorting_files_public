1443858cd894e5565e127a8770aef4d0cd4643be
Lil Yahty <you@example.com>
2023-06-11 17:02:05 +0400

    Первоначальная версия от Maxim

---
+++ sorted.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+directory = "E:\\code\\sort_folders\\foto-filtry-nordfil\\"
+new_directory = "E:\\code\\sort_folders\\foto-filtry-nordfil-go\\"
+
+for file_name in os.listdir(directory):
+    if file_name.endswith(".jpg"):
+        first_word = file_name.split(" ", 1)[0].split("(")[0].split(".")[0].split("_")[0]
+        new_folder = new_directory + first_word
+        if not os.path.exists(new_folder):
+            os.mkdir(new_folder)
+            shutil.copy(directory + file_name, new_folder + "\\" + file_name)
+        else:
+            shutil.copy(directory + file_name, new_folder + "\\" + file_name)
+
