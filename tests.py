ada1c019b593563a38d2976782a56665f4e9754b
Lil Yahty <you@example.com>
2023-06-17 00:35:50 +0400

    Исправил проверку соотвествие папок и расширений

--- tests.py
+++ tests.py
@@ -42,7 +42,7 @@ class TestingSorted(unittest.TestCase):
         for file_name in file_names:
             with open(os.path.join(dir_dirty, file_name), "w"): pass
         number_files_before_sorting = 0
-        for root, dirs, files in os.walk(os.getcwd()):
+        for root, dirs, files in os.walk(dir_dirty):
             number_files_before_sorting += len(files)
         # запускаем сортировку
         dirs_n_extens = (
@@ -58,21 +58,26 @@ class TestingSorted(unittest.TestCase):
         # количество файлов не должно измениться, если изменилось, значит
         # созданы лишние файлы или файлы потеряны
         number_files_after_sorting = 0
-        for root, dirs, files in os.walk(os.getcwd()):
+        for root, dirs, files in os.walk(dir_clean):
             number_files_after_sorting += len(files)
         self.assertEqual(number_files_before_sorting, 
             number_files_after_sorting, msg_number_of_files_changed)
         # в папках для сортировки должны быть только файлы указанных
         # расширений
         for dir_n_extens in dirs_n_extens:
+            # print(f"dir_n_extens: {dir_n_extens}")
             dirname = os.path.join(dir_clean, dir_n_extens["dirname"])
+            # print(f"dirname: {dirname}")
             if not os.path.exists(dirname):
                 continue
             for file in os.listdir(dirname):
-                if not os.path.isfile(file):
+                # print(f"file 1: {file}")
+                filename = os.path.join(dirname, file)
+                if not os.path.isfile(filename):
                     continue
+                # print(f"file 2: {filename}")
                 extensions = dir_n_extens["extensions"]
-                file_extension = file.rsplit(".", 1)[-1]
+                file_extension = filename.rsplit(".", 1)[-1]
                 self.assertIn(file_extension, extensions, 
                     msg_wrong_extension_in_dir)
         # в "грязной" папке, не должно быть файлов, с расширениями
@@ -83,6 +88,24 @@ class TestingSorted(unittest.TestCase):
             file_extension = file.rsplit(".", 1)[-1]
             self.assertNotIn(file_extension, all_extensions, 
                 msg_not_all_sortered)
+
+def sorting_from_to(dir1, dir2, dirs_n_extens):
+    os.mkdir(os.path.join(dir2, "pictures"))
+    os.mkdir(os.path.join(dir2, "text_files"))
+    filename1 = os.path.join(dir2, "pictures/file.jpg")
+    filename2 = os.path.join(dir2, "text_files/file.txt")
+    # filename2 = os.path.join(dir2, "pictures/file.txt")
+    # filename3 = os.path.join(dir2, "text_files/file.jpg")
+    open(filename1, "w").close()
+    # open(filename2, "w").close()
+    # open(filename3, "w").close()
+    #
+    old_filename1 = os.path.join(dir1, "file.jpg")
+    old_filename2 = os.path.join(dir1, "file.txt")
+    os.remove(old_filename1)
+    os.remove(old_filename2)
+    # shutil.rmtree(os.path.join(dir2, "pictures"))
+    pass
 
 
 if __name__ == '__main__':
