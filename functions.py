f11933f3663ab984c3ab5ecde2a0258bc4d0983a
Lil Yahty <you@example.com>
2023-06-17 02:09:47 +0400

    тестирование сортировки, сообщения об ошибке с выводом путей объектов до сортировки и после. Протестировал 3 сценария ошибок

--- functions.py
+++ functions.py
@@ -16,8 +16,19 @@ def function(directory, new_derictory):
 def sorting_from_to(from_dir, to_dir, dirs_n_extens):
     pass
 
+def get_all_paths(dirname):
+    """Возвращает пути всех объектов в каталоге"""
+    all_paths = set()
+    for root, directories, files in os.walk(dirname):
+        for directory in directories:
+            all_paths.add(os.path.join(root, directory))
+        for file in files:
+            all_paths.add(os.path.join(root, file))
+    return sorted(list(all_paths))
+
 
 if __name__ == '__main__':
     directory = "E:\\code\\sort_folders\\foto-filtry-nordfil\\"
     new_directory = "E:\\code\\sort_folders\\foto-filtry-nordfil-go\\"
     function(directory, new_directory)
+    print(get_all_paths(r"D:\files - копия (2)"))
--- tests.py
+++ tests.py
@@ -3,27 +3,27 @@ import os.path
 import shutil
 import unittest
 
-from functions import sorting_from_to
+from functions import sorting_from_to, get_all_paths
 
-msg_number_of_files_changed = """\
-Number of files was change!
-"""
-msg_not_all_sortered = """\
-Not all files sortered!
-"""
-msg_wrong_extension_in_dir = """\
-Wrong extension in directory
-"""
+
+msg_sorting = "Test error: {0}\npaths before:\n{1}\n paths after:\n{2}"
+
+
+def _sorting_error_msg(msg, paths_before, path_after):
+    return "\n\nSorting failed: \"{0}\"\n\n" \
+        "paths before:\n\n{1}\n\n" \
+        "paths after:\n\n{2}\n\n".format(msg, 
+            "\n".join(paths_before), "\n".join(path_after))
+
 
 class TestingSorted(unittest.TestCase):
 
     def setUp(self):
-        # сохраняем начальное значение рабочей директории
-        # нужно для того что бы не было путаницы при запуске из другого
-        # каталога
+        # сохраняем начальное значение рабочей директории нужно для
+        # того что бы не было путаницы при запуске из другого каталога
         self.previous_work_directory = os.getcwd()
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
-        self.dir_for_testing = "for_testing_of_sorting"
+        self.dir_for_testing = "test_dir"
         os.mkdir(self.dir_for_testing)
 
     def tearDown(self):
@@ -41,27 +41,36 @@ class TestingSorted(unittest.TestCase):
         file_names = ("file.txt", "file.jpg")
         for file_name in file_names:
             with open(os.path.join(dir_dirty, file_name), "w"): pass
-        number_files_before_sorting = 0
+        # пути объектов и количество файлов ДО сортировки
+        num_files_before = 0
         for root, dirs, files in os.walk(dir_dirty):
-            number_files_before_sorting += len(files)
+            num_files_before += len(files)
+        paths_before = get_all_paths(self.dir_for_testing)
         # запускаем сортировку
         dirs_n_extens = (
             {"dirname": "pictures", "extensions": ["jpg"]},
             {"dirname": "text_files", "extensions": ["txt"]},
             )
         sorting_from_to(dir_dirty, dir_clean, dirs_n_extens)
+        # пути объектов и количество файлов ПОСЛЕ сортировки
+        paths_after = get_all_paths(self.dir_for_testing)
+        num_files_after = 0
+        for root, dirs, files in os.walk(dir_clean):
+            num_files_after += len(files)
         # проверяем результат сортировки
         all_dirnames, all_extensions = [], []
         for dir_n_extens in dirs_n_extens:
             all_dirnames.append(dir_n_extens["dirname"])
             all_extensions += dir_n_extens["extensions"]
-        # количество файлов не должно измениться, если изменилось, значит
-        # созданы лишние файлы или файлы потеряны
-        number_files_after_sorting = 0
-        for root, dirs, files in os.walk(dir_clean):
-            number_files_after_sorting += len(files)
-        self.assertEqual(number_files_before_sorting, 
-            number_files_after_sorting, msg_number_of_files_changed)
+        # в "грязной" папке, не должно быть файлов, с расширениями
+        # для сортировки
+        for file in os.listdir(dir_dirty):
+            if not os.path.isfile(os.path.join(dir_dirty, file)):
+                continue
+            file_extension = file.rsplit(".", 1)[-1]
+            self.assertNotIn(file_extension, all_extensions, 
+                _sorting_error_msg("Not all files sortered!",
+                        paths_before, paths_after))
         # в папках для сортировки должны быть только файлы указанных
         # расширений
         for dir_n_extens in dirs_n_extens:
@@ -78,16 +87,14 @@ class TestingSorted(unittest.TestCase):
                 # print(f"file 2: {filename}")
                 extensions = dir_n_extens["extensions"]
                 file_extension = filename.rsplit(".", 1)[-1]
-                self.assertIn(file_extension, extensions, 
-                    msg_wrong_extension_in_dir)
-        # в "грязной" папке, не должно быть файлов, с расширениями
-        # для сортировки
-        for file in os.listdir(dir_dirty):
-            if not os.path.isfile(os.path.join(dir_dirty, file)):
-                continue
-            file_extension = file.rsplit(".", 1)[-1]
-            self.assertNotIn(file_extension, all_extensions, 
-                msg_not_all_sortered)
+                self.assertIn(file_extension, extensions,
+                    _sorting_error_msg("Wrong file extension in directory",
+                        paths_before, paths_after))
+        # количество файлов не должно измениться, если изменилось, значит
+        # созданы лишние файлы или файлы потеряны
+        self.assertEqual(num_files_before, num_files_after, 
+            _sorting_error_msg("Number of files was changed",
+                paths_before, paths_after))
 
 def sorting_from_to(dir1, dir2, dirs_n_extens):
     os.mkdir(os.path.join(dir2, "pictures"))
@@ -95,10 +102,10 @@ def sorting_from_to(dir1, dir2, dirs_n_extens):
     filename1 = os.path.join(dir2, "pictures/file.jpg")
     filename2 = os.path.join(dir2, "text_files/file.txt")
     # filename2 = os.path.join(dir2, "pictures/file.txt")
-    # filename3 = os.path.join(dir2, "text_files/file.jpg")
+    filename3 = os.path.join(dir2, "text_files/file1.txt")
     open(filename1, "w").close()
-    # open(filename2, "w").close()
-    # open(filename3, "w").close()
+    open(filename2, "w").close()
+    open(filename3, "w").close()
     #
     old_filename1 = os.path.join(dir1, "file.jpg")
     old_filename2 = os.path.join(dir1, "file.txt")
