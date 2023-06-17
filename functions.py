0dd3563a1faec4d9b49783b54c56f0f5d61e35bd
Lil Yahty <you@example.com>
2023-06-18 00:18:19 +0400

    Сделал функцию сортировки, тест проходит

--- functions.py
+++ functions.py
@@ -1,5 +1,8 @@
 import os
+import os.path
 import shutil
+from pprint import pprint
+
 
 def function(directory, new_derictory):
     for file_name in os.listdir(directory):
@@ -13,8 +16,52 @@ def function(directory, new_derictory):
                 shutil.copy(directory + file_name, new_folder + "\\" + file_name)
 
 
-def sorting_from_to(from_dir, to_dir, dirs_n_extens):
-    pass
+def sorting_from_to(from_dir, to_dir, dirs_n_extens, subdirs=False):
+    """Сортируем файлы в папки с указанными для этиз папок расширениями
+        полные пути к папкам:
+        from_dir - папка с неотсортированными файлами
+        to_dir - папка куда нужно сортировать
+        dirs_n_extens - правила сортировки
+        subdirs - искать ли файлы в подпапках
+    """
+    # собираем полные имена всех файлов которые подлежат сортировке
+    all_extensions = []
+    for dir_n_extens in dirs_n_extens:
+        all_extensions += dir_n_extens["extensions"]
+    filenames_for_sorting = []
+    for root, dirs, files in os.walk(from_dir):
+        for file in files:
+            filename = os.path.join(root, file)
+            file_extension = filename.rsplit(".", 1)[-1]
+            if file_extension in all_extensions:
+                filenames_for_sorting.append(filename)
+        # не собираем файлы из подпапок если это не нужно
+        if not subdirs:
+            break
+    # создаём новые пути файлов на основе старых
+    old_new_filenames = []
+    for filename in filenames_for_sorting:
+        for dir_n_extens in dirs_n_extens:
+            file_extension = filename.rsplit(".", 1)[-1]
+            if file_extension in dir_n_extens["extensions"]:
+                dirname = os.path.join(to_dir, dir_n_extens["dirname"])
+                if not os.path.exists(dirname):
+                    os.mkdir(dirname)
+                new_filename = get_new_filename(filename, dirname)
+                old_new_filenames.append((filename, new_filename))
+                # print((filename, new_filename))
+    # копируем файл в новое место, и удаляем файл из старого места
+    for old_filename, new_filename in old_new_filenames:
+        shutil.copy(old_filename, new_filename)
+        os.remove(old_filename)
+    # pprint(vars())
+
+
+def get_new_filename(filename, dirname):
+    """Создаём новое полное имя для файла, для указанной директории"""
+    file = os.path.basename(filename)
+    return os.path.join(dirname, file)
+
 
 def get_all_paths(dirname):
     """Возвращает пути всех объектов в каталоге"""
@@ -30,4 +77,13 @@ def get_all_paths(dirname):
 if __name__ == '__main__':
     directory = "E:\\code\\sort_folders\\foto-filtry-nordfil\\"
     new_directory = "E:\\code\\sort_folders\\foto-filtry-nordfil-go\\"
-    function(directory, new_directory)
+    # function(directory, new_directory)
+    dirs_n_extens = (
+        {"dirname": "docs", "extensions": ["docx"]},
+        {"dirname": "pictures", "extensions": ["jpg"]},
+        )
+    dir_dirty = r"D:\dir_dirty"
+    dir_clean = r"D:\dir_clean"
+    sorting_from_to(dir_dirty, dir_clean, dirs_n_extens, True)
+    # res = get_new_filename((r"D:\dir_dirty\f1.docx", dir_clean))
+    # print(f"get_new_filename(): {res}")
